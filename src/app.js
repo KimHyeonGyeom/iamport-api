65cfbb243aec2fb963812a1896c029a0176c39c3
hyeon <couse6610@gmail.com>
2022-01-03 22:32:14 +0900

    iamport 결제 테스트

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 import express from 'express';
-const app = express();
-
+import path  from 'path';
 
 const port = 4000;
+const __dirname = path.resolve();
+const app = express();
+
+app.get('/',function(req,res){
+    // 응답.send('여기는 나의 작업용 페이지입니다.');
+    res.sendFile(__dirname + '/pay.html');
+});
+
+app.post('/webhook',function(req,res){
+    // 응답.send('여기는 나의 작업용 페이지입니다.');
+   console.log('webhook test');
+   res.send({status : 200});
+});
+
 app.listen(port, () => {
     console.log(`App listening on the port ${port}`);
 });
